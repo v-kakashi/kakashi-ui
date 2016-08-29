f9aa252072ef3778cb4b723234bc9f57323c202a
limingmin <314407103@qq.com>
2016-08-29 10:47:20 +0800

    Updates

--- examples/install.js
+++ examples/install.js
@@ -1,9 +1,2 @@
-webpackJsonp([15,0],[
-/* 0 */
-/***/ function(module, exports) {
-
-	"use strict";
-
-/***/ }
-]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhhbXBsZXMvaW5zdGFsbC5qcyIsInNvdXJjZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7OzsiLCJzb3VyY2VSb290IjoiIn0=
+webpackJsonp([15,0],[function(c,n){"use strict"}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZXhhbXBsZXMvaW5zdGFsbC5qcyIsInNvdXJjZXMiOlsid2VicGFjazovLy9leGFtcGxlcy9pbnN0YWxsLmpzIl0sIm1hcHBpbmdzIjoiQUFBQSIsInNvdXJjZVJvb3QiOiIifQ==
